ba1a6290a0b73e3d92dcf83de7d433cf4adcbcc7
Niels Bassler <niels.bassler@gmail.com>
2026-01-17 21:57:04 +0100

    osh_logger: new thread-safe  logger  (WIP)

--- src/common/osh_logger.h
+++ src/common/osh_logger.h
@@ -1,142 +1,179 @@
-#ifndef _OSH_LOGGER
-#define _OSH_LOGGER
+/* osh_logger.h - portable, thread-safe, re-entrant logger (C)
+ *
+ * Goals:
+ *  - re-entrant (no recursion inside logger impl)
+ *  - thread-safe
+ *  - builds on POSIX + Windows (MinGW)
+ *  - minimal surface area
+ *  - no user-defined typedefs
+ */
 
-#include "osh_exit.h"
+#ifndef OSH_LOGGER_H
+#define OSH_LOGGER_H
 
-enum {
-    OSH_LOG_OFF = 0,
-    OSH_LOG_FATAL,
-    OSH_LOG_ERR,
-    OSH_LOG_WARN,
-    OSH_LOG_INFO, /* default level */
-    OSH_LOG_DEBUG,
-    OSH_LOG_TRACE
+#include <stdarg.h>
+#include <stddef.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* -----------------------------
+ * Levels
+ * ----------------------------- */
+enum osh_log_level {
+    OSH_LOG_TRACE = 0,
+    OSH_LOG_DEBUG = 1,
+    OSH_LOG_INFO = 2,
+    OSH_LOG_WARN = 3,
+    OSH_LOG_ERROR = 4,
+    OSH_LOG_FATAL = 5,
+    OSH_LOG_OFF = 6
 };
 
-/* prefix will prepended any string used by the corresponding OSH_ log function
+const char *osh_log_level_name(int level);
+
+/* -----------------------------
+ * Flags (bitmask)
+ * ----------------------------- */
+enum osh_log_flag {
+    OSH_LOG_F_NONE = 0u,
+    OSH_LOG_F_TIMESTAMP = 1u << 0,
+    OSH_LOG_F_THREAD_ID = 1u << 1,
+    OSH_LOG_F_FILELINE = 1u << 2,
+    OSH_LOG_F_FUNCTION = 1u << 3
+};
+
+/* -----------------------------
+ * Opaque logger handle
+ * ----------------------------- */
+struct osh_logger;
+
+/* -----------------------------
+ * Default global logger (keeps existing style)
+ * ----------------------------- */
+
+/* Create default logger:
+ *  - always logs to stderr
+ *  - level and flags set from args
+ * Returns 0 on success, nonzero on error.
  */
-#define OSH_LOG_PREFIX_ERROR "*** Error: "
-#define OSH_LOG_PREFIX_WARN "*** Warning: "
-#define OSH_LOG_PREFIX_INFO "  "
-#define OSH_LOG_PREFIX_DEBUG "Debug: "
-#define OSH_LOG_PREFIX_LOG ""
-#define OSH_LOG_HLINE "-----------------------------------------------------\n"
+int osh_log_init(int level, unsigned flags);
 
-#define OSH_LOG_REPEAT 10 /* how often a message is repeated before it is suppressed */
+/* Close default logger and free all resources (idempotent). */
+void osh_log_close(void);
 
-/**
- * @brief Setup the logger for OpenShieldHIT
- *
- * @param[in] path - path to logfile
- * @param[in] log_level - see defines in osh_logger.h
- *
- * @returns 1 if successful, 0 if logger was already setup
- *
- * @warning This is a temporary solution, since it is not thread-safe.
- *
- * @author Niels Bassler
+/* Optional sinks on the default logger */
+int osh_log_add_file(const char *path, int append);
+int osh_log_enable_stdout(int enable); /* 0/1, for INFO+ for example; implementation decides */
+int osh_log_set_level(int level);
+int osh_log_get_level(void);
+void osh_log_set_flags(unsigned flags);
+unsigned osh_log_get_flags(void);
+void osh_log_flush(void);
+
+/* -----------------------------
+ * Explicit logger instances (optional but useful for tests / libraries)
+ * ----------------------------- */
+struct osh_logger *osh_logger_create(int level, unsigned flags);
+void osh_logger_destroy(struct osh_logger *lg);
+
+int osh_logger_add_file(struct osh_logger *lg, const char *path, int append);
+int osh_logger_enable_stdout(struct osh_logger *lg, int enable);
+void osh_logger_set_level(struct osh_logger *lg, int level);
+int osh_logger_get_level(const struct osh_logger *lg);
+void osh_logger_set_flags(struct osh_logger *lg, unsigned flags);
+unsigned osh_logger_get_flags(const struct osh_logger *lg);
+void osh_logger_flush(struct osh_logger *lg);
+
+/* Optional: user callback sink (no vtable, just one callback) */
+typedef void (*osh_log_write_cb)(void *user, const char *msg, size_t len);
+int osh_logger_set_callback(struct osh_logger *lg, osh_log_write_cb cb, void *user);
+
+/* -----------------------------
+ * Canonical logging entry points
+ * ----------------------------- */
+
+/* Extended form allows attaching file/line/function explicitly.
+ * flags_override: if 0, uses logger flags. If nonzero, uses that value.
  */
-int osh_setup_logger(char const *path, int log_level);
+void osh_logger_logv_ex(struct osh_logger *lg, int level, unsigned flags_override, const char *file, int line,
+                        const char *function, const char *fmt, va_list ap);
 
-/**
- * @brief Shutdown the logger for OpenShieldHIT.
- *
- * @returns 0 if successful
- *
- * @author Niels Bassler
- */
-int osh_close_logger(void);
+void osh_logger_log_ex(struct osh_logger *lg, int level, unsigned flags_override, const char *file, int line,
+                       const char *function, const char *fmt, ...)
+#if defined(__GNUC__) || defined(__clang__)
+    __attribute__((format(printf, 7, 8)))
+#endif
+    ;
 
-/**
- * @brief set the logger level
- *
- * @param log_level : logger level e.g. osh_LOG_WARN
- * @returns 0 if successful, 1 if unsuccessful
- *
- * @author Niels Bassler
- */
-int osh_set_loglevel(int log_level);
+/* Plain form without file/line/function */
+void osh_logger_logv(struct osh_logger *lg, int level, const char *fmt, va_list ap);
+void osh_logger_log(struct osh_logger *lg, int level, const char *fmt, ...)
+#if defined(__GNUC__) || defined(__clang__)
+    __attribute__((format(printf, 3, 4)))
+#endif
+    ;
 
-/**
- * @brief get current logger level
- *
- * @returns current logging level
- *
- * @author Niels Bassler
- */
-int osh_get_loglevel(void);
+/* Default logger convenience functions (useful when migrating existing code) */
+void osh_trace(const char *fmt, ...)
+#if defined(__GNUC__) || defined(__clang__)
+    __attribute__((format(printf, 1, 2)))
+#endif
+    ;
+void osh_debug(const char *fmt, ...)
+#if defined(__GNUC__) || defined(__clang__)
+    __attribute__((format(printf, 1, 2)))
+#endif
+    ;
+void osh_info(const char *fmt, ...)
+#if defined(__GNUC__) || defined(__clang__)
+    __attribute__((format(printf, 1, 2)))
+#endif
+    ;
+void osh_warn(const char *fmt, ...)
+#if defined(__GNUC__) || defined(__clang__)
+    __attribute__((format(printf, 1, 2)))
+#endif
+    ;
+void osh_error(const char *fmt, ...)
+#if defined(__GNUC__) || defined(__clang__)
+    __attribute__((format(printf, 1, 2)))
+#endif
+    ;
 
-/**
- * @brief Print message to STDERR (and logfile if configured) and terminate.
- *
- * Appends a newline automatically. Accepts printf-style format arguments.
- *
- * @param[in] status Process exit code (use EX_* from osh_exit.h).
- * @param[in] msg    Null-terminated format string.
- * @param[in] ...    Optional printf-style arguments.
- *
- * @author Niels Bassler
- */
-void osh_err(int status, char const *msg, ...);
+/* Logs then exits(exit_code). noreturn attribute is best-effort. */
+void osh_fatal(int exit_code, const char *fmt, ...)
+#if defined(__GNUC__) || defined(__clang__)
+    __attribute__((format(printf, 2, 3), noreturn))
+#endif
+    ;
 
-/**
- * @brief Print message to STDERR and exit with EX_UNAVAILABLE.
- * Write also to the logfile, if it was setup.
- *
- * Message must be terminated with a null byte: //CHAR(0)
- * Message will automatically be terminated with a newline.
- *
- * @param[in] msg - Null-terminated format string.
- * @param[in] ... Optional format arguments
- *
- * @author Niels Bassler
- */
-void osh_malloc_err(char const *msg, ...);
+/* -----------------------------
+ * Macro layer capturing file/line/function
+ * ----------------------------- */
+#ifndef OSH_LOG_USE_FILELINE
+#define OSH_LOG_USE_FILELINE 1
+#endif
 
-/**
- * @brief Print a warning to stdout and logfile, if it was setup.
- *
- * Message must be terminated with a null byte: //CHAR(0)
- *
- * @param[in] msg - Null-terminated format string.
- * @param[in] ... Optional format arguments
- *
- * @author Niels Bassler
- */
-void osh_warn(char const *msg, ...);
+/* Accessor for default logger (may return NULL if not initialized) */
+struct osh_logger *osh_log_default(void);
 
-/**
- * @brief Write a message to stdout _and_ the logfile, if it was setup.
- *
- * Message must be terminated with a null byte: //CHAR(0)
- * @param[in] msg - Null-terminated format string.
- * @param[in] ... Optional format arguments
- *
- * @author Niels Bassler
- */
-void osh_info(char const *msg, ...);
+#if OSH_LOG_USE_FILELINE
+#define OSH_LOG(lvl, fmt, ...)                                                                                         \
+    osh_logger_log_ex(osh_log_default(), (lvl), 0u, __FILE__, __LINE__, __func__, (fmt), ##__VA_ARGS__)
+#else
+#define OSH_LOG(lvl, fmt, ...) osh_logger_log(osh_log_default(), (lvl), (fmt), ##__VA_ARGS__)
+#endif
 
-/**
- * @brief Write a message to stdout _and_ the logfile, if it was setup.
- *
- * Message must be terminated with a null byte: //CHAR(0)
- * @param[in] msg - Null-terminated format string.
- * @param[in] ... Optional format arguments
- *
- * @author Niels Bassler
- */
-void osh_debug(char const *msg, ...);
+#define OSH_TRACE(fmt, ...) OSH_LOG(OSH_LOG_TRACE, (fmt), ##__VA_ARGS__)
+#define OSH_DEBUG(fmt, ...) OSH_LOG(OSH_LOG_DEBUG, (fmt), ##__VA_ARGS__)
+#define OSH_INFO(fmt, ...) OSH_LOG(OSH_LOG_INFO, (fmt), ##__VA_ARGS__)
+#define OSH_WARN(fmt, ...) OSH_LOG(OSH_LOG_WARN, (fmt), ##__VA_ARGS__)
+#define OSH_ERROR(fmt, ...) OSH_LOG(OSH_LOG_ERROR, (fmt), ##__VA_ARGS__)
 
-/**
- * @brief Write a message to the logfile, if it was setup. Do not output to
- * stdout.
- *
- * Message must be terminated with a null byte: //CHAR(0)
- * @param[in] msg - Null-terminated format string.
- * @param[in] ... Optional format arguments
- *
- * @author Niels Bassler
- */
-void osh_log(char const *msg, ...);
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
 
-#endif /* !_OSH_LOGGER */
+#endif /* OSH_LOGGER_H */
--- tests/test_osh_logger.c
+++ tests/test_osh_logger.c
@@ -5,49 +5,74 @@
 
 #include "osh_logger.h"
 
-void test_logger_basic() {
-    const char *logfile = "test_logger.log";
-    char line[512];
-    FILE *f;
-    int found_info = 0;
-    int found_warn = 0;
-
-    /* Setup logger */
-    assert(osh_setup_logger(logfile, OSH_LOG_INFO) == 1);
-
-    /* Test log level */
-    assert(osh_get_loglevel() == OSH_LOG_INFO);
-    assert(osh_set_loglevel(OSH_LOG_WARN) == 0);
-    assert(osh_get_loglevel() == OSH_LOG_WARN);
-
-    /* Write some logs */
-    osh_info("Info message: %d", 42);
-    osh_warn("Warning message: %s", "be careful");
-
-    /* Close logger */
-    assert(osh_close_logger() == 0);
-
-    /* Check if file exists and contains something */
-    f = fopen(logfile, "r");
-    assert(f != NULL);
+static int file_contains(const char *path, const char *needle) {
+    char line[1024];
+    FILE *f = fopen(path, "r");
+    if (!f)
+        return 0;
 
     while (fgets(line, sizeof(line), f)) {
-        if (strstr(line, "Info message"))
-            found_info = 1;
-        if (strstr(line, "Warning message"))
-            found_warn = 1;
+        if (strstr(line, needle)) {
+            fclose(f);
+            return 1;
+        }
     }
     fclose(f);
+    return 0;
+}
 
-    /* Written logs alsway has info level irrespective of log level */
-    assert(found_info == 1);
-    assert(found_warn == 1);
+static int file_not_contains(const char *path, const char *needle) {
+    return !file_contains(path, needle);
+}
+
+void test_logger_basic(void) {
+    const char *logfile = "test_logger.log";
+
+    /* Start clean */
+    remove(logfile);
+
+    /* Init default logger */
+    assert(osh_log_init(OSH_LOG_INFO, OSH_LOG_F_NONE) == 0);
+
+    /* Add file sink */
+    assert(osh_log_add_file(logfile, /*append=*/0) == 0);
+
+    /* Level getters/setters */
+    assert(osh_log_get_level() == OSH_LOG_INFO);
+    assert(osh_log_set_level(OSH_LOG_WARN) == 0);
+    assert(osh_log_get_level() == OSH_LOG_WARN);
+
+    /* With level WARN: INFO should be suppressed, WARN should appear */
+    osh_info("Info message (should NOT appear): %d", 42);
+    osh_warn("Warning message A: %s", "be careful");
+
+    /* Lower threshold to INFO so INFO appears */
+    assert(osh_log_set_level(OSH_LOG_INFO) == 0);
+    osh_info("Info message (should appear): %d", 43);
+    osh_warn("Warning message B: %s", "still careful");
+
+    /* Ensure everything is written */
+    osh_log_flush();
+    osh_log_close();
+
+    /* Verify file exists */
+    {
+        FILE *f = fopen(logfile, "r");
+        assert(f != NULL);
+        fclose(f);
+    }
+
+    /* Verify contents */
+    assert(file_contains(logfile, "Warning message A"));
+    assert(file_contains(logfile, "Warning message B"));
+    assert(file_contains(logfile, "Info message (should appear)"));
+    assert(file_not_contains(logfile, "Info message (should NOT appear)"));
 
     /* Clean up */
     remove(logfile);
 }
 
-int main() {
+int main(void) {
     test_logger_basic();
     printf("Logger test passed.\n");
     return 0;
