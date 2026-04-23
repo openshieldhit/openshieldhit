b6df22d74340ea67c340ea869fec640a891b8e56
Niels Bassler <bassler@clin.au.dk>
2026-04-23 19:00:33 +0200

    avoid loading HU tables when not needed

--- tests/unit/test_osh_vect.c
+++ tests/unit/test_osh_vect.c
@@ -1,13 +1,13 @@
-#include <assert.h>
 #include <math.h>
 #include <stdio.h>
 
 #include "common/osh_vect.h"
+#include "test_assert.h"
 
 void test_dot_product(void) {
     double a[OSH_VECT_DIM] = {1.0, 2.0, 3.0};
     double b[OSH_VECT_DIM] = {4.0, -5.0, 6.0};
-    assert(fabs(osh_vect_dot(a, b) - (1 * 4 + 2 * (-5) + 3 * 6)) < OSH_VECT_EPS);
+    ASSERT_TRUE(fabs(osh_vect_dot(a, b) - (1 * 4 + 2 * (-5) + 3 * 6)) < OSH_VECT_EPS);
 }
 
 void test_cross_product(void) {
@@ -15,19 +15,19 @@ void test_cross_product(void) {
     double j[OSH_VECT_DIM] = {0.0, 1.0, 0.0};
     double k[OSH_VECT_DIM];
     osh_vect_cross(i, j, k);
-    assert(fabs(k[0] - 0.0) < OSH_VECT_EPS);
-    assert(fabs(k[1] - 0.0) < OSH_VECT_EPS);
-    assert(fabs(k[2] - 1.0) < OSH_VECT_EPS);
+    ASSERT_TRUE(fabs(k[0] - 0.0) < OSH_VECT_EPS);
+    ASSERT_TRUE(fabs(k[1] - 0.0) < OSH_VECT_EPS);
+    ASSERT_TRUE(fabs(k[2] - 1.0) < OSH_VECT_EPS);
 }
 
 void test_norm(void) {
     double u[OSH_VECT_DIM] = {3.0, 4.0, 0.0};
     double v[OSH_VECT_DIM];
-    assert(fabs(osh_vect_len2(u) - 25.0) < OSH_VECT_EPS); /* 3**2 + 4**2 = 25 */
+    ASSERT_TRUE(fabs(osh_vect_len2(u) - 25.0) < OSH_VECT_EPS); /* 3**2 + 4**2 = 25 */
 
     osh_vect_norm2(u, v); /* normalize u into v */
 
-    assert(fabs(osh_vect_len2(v) - 1.0) < OSH_VECT_EPS); /* should be unit vector */
+    ASSERT_TRUE(fabs(osh_vect_len2(v) - 1.0) < OSH_VECT_EPS); /* should be unit vector */
 }
 
 void test_affine_bzalign_transform(void) {
@@ -43,13 +43,13 @@ void test_affine_bzalign_transform(void) {
     osh_vect_trans_point_affine(p_local, p_world, tm);
     osh_vect_trans_vector_affine(v_local, v_world, tm);
 
-    assert(fabs(p_world[0] - 5.0) < OSH_VECT_EPS);
-    assert(fabs(p_world[1] - 7.0) < OSH_VECT_EPS);
-    assert(fabs(p_world[2] - 9.0) < OSH_VECT_EPS);
+    ASSERT_TRUE(fabs(p_world[0] - 5.0) < OSH_VECT_EPS);
+    ASSERT_TRUE(fabs(p_world[1] - 7.0) < OSH_VECT_EPS);
+    ASSERT_TRUE(fabs(p_world[2] - 9.0) < OSH_VECT_EPS);
 
-    assert(fabs(v_world[0] - 0.0) < OSH_VECT_EPS);
-    assert(fabs(v_world[1] - 0.0) < OSH_VECT_EPS);
-    assert(fabs(v_world[2] - 1.0) < OSH_VECT_EPS);
+    ASSERT_TRUE(fabs(v_world[0] - 0.0) < OSH_VECT_EPS);
+    ASSERT_TRUE(fabs(v_world[1] - 0.0) < OSH_VECT_EPS);
+    ASSERT_TRUE(fabs(v_world[2] - 1.0) < OSH_VECT_EPS);
 }
 
 int main(void) {
